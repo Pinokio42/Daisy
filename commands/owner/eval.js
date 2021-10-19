cf0a3bd8f5fd386d7b83f49c67b5520011b04b45
Pinokio42 <79786820+Pinokio42@users.noreply.github.com>
2021-10-19 14:37:03 +0700

    Update eval.js

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const config = require('../../config.js');
+const config = require('../../Chief/.js');
 const beautify = require('beautify');
 
 module.exports = {
